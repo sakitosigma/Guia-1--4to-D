f7ff83731292ecc9aad1f0053d0798518de2d1de
sakitosigma <49009308@est.ort.edu.ar>
2025-03-17 10:41:22 -0300

    clase 1 finish

--- Assets/scripts/DeclaracionDeVariables.cs
+++ Assets/scripts/DeclaracionDeVariables.cs
@@ -6,17 +6,26 @@ public class DeclaracionDeVariables : MonoBehaviour
 {
     int edadusuario;
     float sueldoUsuario;
+    string domicilio;
     bool aprobo;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        edadusuario = 16;
+        sueldoUsuario = 200.000F;
+        aprobo = true;
+        domicilio = "Libertador 1369";
+
+        Debug.Log(edadusuario);
+        Debug.Log(sueldoUsuario);
+        Debug.Log(domicilio);
+        Debug.Log(aprobo);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
