26de70336a1e81cdc2485f4073f6501b407ad519
sakitosigma <49009308@est.ort.edu.ar>
2025-03-31 10:43:30 -0300

    guia 2 num1

---
+++ Assets/scripts/num1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class num1 : MonoBehaviour
+{
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num2 += 2;
+        Debug.Log(num2 * num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
