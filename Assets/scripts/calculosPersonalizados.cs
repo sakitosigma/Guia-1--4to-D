85e798e1c4a56b63c9f6182c5202a528e7136873
sakitosigma <49009308@est.ort.edu.ar>
2025-03-31 10:25:10 -0300

    guia 1 lista

---
+++ Assets/scripts/calculosPersonalizados.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class calculosPersonalizados : MonoBehaviour
+{
+    public float num1;
+    public float num2;
+    public float ResultadoSuma;
+    public float ResultadoResta;
+    public float ResultadoMultiplicacion;
+    public float ResultadoDivision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResultadoSuma = num1 + num2;
+        ResultadoResta = num1 - num2;
+        ResultadoMultiplicacion = num1 * num2;
+        ResultadoDivision = num1 / num2;
+        Debug.Log("La suma entre " + num1 + " y " + num2 + " es " + ResultadoSuma);
+        Debug.Log("La resta entre " + num1 + " y " + num2 + " es " + ResultadoResta);
+        Debug.Log("La multiplicacion entre " + num1 + " y " + num2 + " es " + ResultadoMultiplicacion);
+        Debug.Log("La divicion entre " + num1 + " y " + num2 + " es " + ResultadoDivision);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/guia1ej2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guia1ej2 : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edad;
+    public float estatura;
+    public bool donanteOrgano;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edad + " años " + ", mido " + estatura + " y " + donanteOrgano + " es donante de organos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
