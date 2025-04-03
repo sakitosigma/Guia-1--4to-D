58efacb7e6ccb3063a61809fcd195aa7faf776ac
sakitosigma <49009308@est.ort.edu.ar>
2025-04-03 10:15:39 -0300

    ejercicio6

---
+++ Assets/scripts/parImpar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parImpar : MonoBehaviour
+{
+    public int num1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 > 0)
+        {
+            if(num1 % 2 == 0)
+            {
+                Debug.Log("El numero es par");
+            } else
+            {
+                Debug.Log("El numero es impar");
+            }
+        } else
+        {
+            Debug.Log("el numero no es mayor a 0");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
